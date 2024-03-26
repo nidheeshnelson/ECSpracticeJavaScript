f81bf39c214ba7eab4fbf27c8dee9e6a86643dd0
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-03-26 12:10:31 +0530

    arithmatic commit

---
+++ first.js
@@ -0,0 +1,15 @@
+let a = 50;console.log(a);
+a=a+20;console.log(a);
+a=a-6;console.log(a);
+a=a*4;console.log(a);
+a=a/2;console.log(a);
+a=a%5;console.log(a);
+a+=20;console.log(a);
+a-=10;console.log(a);;
+a*=9;console.log(a);
+a/=3;console.log(a);
+a%=29;console.log(a);
+a**=2;console.log(a);
+a++;console.log(a);
+a--;console.log(a);
+
