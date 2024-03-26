d23002c4991bd1f6ecf527ff81493b206eb2fd94
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-03-26 11:09:21 +0530

    basic commit

---
+++ first.js
@@ -0,0 +1,5 @@
+console.log(`Hello JS`);
+console.log(`I am Here`);
+window.alert(`This is an alert`)
+document.getElementById("h").textContent = `Hii JS`;
+document.getElementById("p").textContent = `You are Stylish`;
