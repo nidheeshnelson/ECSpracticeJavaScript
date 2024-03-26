ab5785aedd63d338e589eb25333eb6adb5f5254a
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-03-26 14:46:28 +0530

    built in object

---
+++ first.js
@@ -0,0 +1,8 @@
+let b=36.25;
+let a=Math.floor(b);console.log(a);
+a=Math.ceil(b);console.log(a);
+a=Math.round(b);console.log(a);
+a=Math.pow(b,2);console.log(a);
+a=Math.sqrt(b);console.log(a);
+a=Math.max(25,14,50,89,32);console.log(a);
+a=Math.min(14,52,78,5,98);console.log(a);
