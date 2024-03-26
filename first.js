ebe3e994880638418c7b17c68e14800b70586e13
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-03-26 11:50:43 +0530

    variable commit

---
+++ first.js
@@ -0,0 +1,15 @@
+let a = 2000;
+let b = 1.5;
+let c = 85;
+let d ="Nidheesh Nelson";
+console.log(`this is the year ${a} and $${b} values rupees ${c} and your name is ${d}`)
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+let g = 25;
+let j = true;
+console.log(typeof j)
+document.getElementById("1").textContent=`Your name is ${d}`;
+document.getElementById("2").textContent=`Your age is ${g}`;
+document.getElementById("3").textContent=`Your employed ${j}`;
