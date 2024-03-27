eaee3b764e10bc62c21b8690de82d3f0643dc5bc
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-03-27 10:59:44 +0530

    while commit

---
+++ first.js
@@ -0,0 +1,9 @@
+let a=true;
+let u;
+let p;
+do{
+u=window.prompt("Enter User name")
+p=window.prompt("enter password")
+u==="user"&&p==="pass"?window.alert("You are logged in"):window.alert("You are not logged in");
+}
+while(a);
