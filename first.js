2e76459596af709e4612e296261257533b1d3fd4
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-03-27 13:36:40 +0530

    rest commit

---
+++ first.js
@@ -0,0 +1,10 @@
+function getFood(...f){
+return f;
+}
+let food1 = "rice";
+let food2 = "Biri";
+let food3 = "tapioka";
+let food4 = "pazha";
+let food5 = "kanji";
+let food6 = "soop";
+console.log(getFood(food1,food2,food3,food4,food5,food6));
