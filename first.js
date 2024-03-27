a0053ff3be3f73de2688cb23cdb8926aec9bc0f3
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-03-27 11:17:01 +0530

    for commit

---
+++ first.js
@@ -0,0 +1,12 @@
+for(let i=0;i<=20;i+=3){
+    if(i===3){
+        console.log("It is 3");
+        continue
+    }
+    console.log(i);
+    if(i===15){
+        console.log("15 so ending");
+        break;
+    }
+}
+console.log("Happy...")
