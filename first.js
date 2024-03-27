5597ebc242466d98ec85b236061f318a9488ae56
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-03-27 16:52:32 +0530

    objects commit

---
+++ first.js
@@ -0,0 +1,16 @@
+const p1={
+    fn:"Nidheesh",
+    ln:"Nelson",
+    ag:32,
+    hi:()=>console.log("I am fine....")
+}
+const p2={
+    fn:"Braji",
+    ln:"Nidheesh",
+    ag:26,
+    hi:()=>console.log("I am good...")
+}
+p1.fn;
+p2.fn;
+p1.hi();
+p2.hi();
