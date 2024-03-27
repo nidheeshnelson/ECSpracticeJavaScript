18c7a5550538f47fb422983422a86f44271c2bbf
nidheeshnelson <nidheeshnelson10@gmail.com>
2024-03-27 10:42:35 +0530

    logic compare commit

---
+++ first.js
@@ -0,0 +1,9 @@
+let pi = 3.14;
+if(pi==="3.14" || pi=="3.14"){
+    console.log("Farely currect");
+}
+else{console.log("Farely not currect");}
+if(pi==="3.14" && pi=="3.14"){
+    console.log("Strictly currect");
+}
+else{console.log("Strictly not currect");}
